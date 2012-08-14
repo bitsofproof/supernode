b18f7f206fe7834f700f4fcde8753ec631865fc4
aheadofthecurve <aheadofthecurve@gmx.com>
2012-08-14 19:30:36 +0200

    runs with securedb

--- src/org/purser/server/Tester.java
+++ src/org/purser/server/Tester.java
@@ -1,5 +1,11 @@
 package org.purser.server;
 
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -21,11 +27,38 @@ public class Tester {
 		entityManager.flush();
 		return order.getId();
 	}
-
-	public static void main (String [] args)
+	
+	static String getPassword () throws IOException
 	{
+		  String password = null;
+		  File master = new File ("MASTERPASSWORD");
+		  if ( master.exists() )
+		  {
+			  FileReader reader = new FileReader(master);
+			  password = new BufferedReader(reader).readLine();
+			  reader.close();
+		  }
+		  if ( password == null )
+		  {
+			  Console console = System.console();
+			    if (console == null) {
+			      throw new IOException ("unable to obtain console");
+			    }
+
+			    password = new String(console.readPassword("MASTERPASSWORD: "));
+		  }
+		  return password;
+	}
+
+	public static void main (String [] args) throws IOException
+	{
+
+		String password = Tester.getPassword ();
+		  System.setProperty("javax.net.ssl.keyStorePassword", password);
+		  System.setProperty("javax.net.ssl.trustStorePassword", password);
+
 		ApplicationContext context = 
-	            new ClassPathXmlApplicationContext("resources/app-context.xml");
+	            new ClassPathXmlApplicationContext("app-context.xml");
 		try {
 			Tester p = context.getBean(Tester.class);
 			p.testSaveOrderWithItems();
