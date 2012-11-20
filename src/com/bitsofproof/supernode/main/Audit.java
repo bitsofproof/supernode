9649e8bad897a615e2c0420f0cc37289d4b6071a
bitsofproof <tamas@bitsofproof.com>
2012-11-20 08:46:44 +0100

    started audit coding

--- src/com/bitsofproof/supernode/main/Audit.java
+++ src/com/bitsofproof/supernode/main/Audit.java
@@ -16,6 +16,11 @@
 package com.bitsofproof.supernode.main;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.GnuParser;
@@ -23,14 +28,36 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
+import com.bitsofproof.supernode.core.Chain;
+import com.bitsofproof.supernode.model.Blk;
+import com.bitsofproof.supernode.model.Head;
+import com.bitsofproof.supernode.model.QBlk;
+import com.bitsofproof.supernode.model.QHead;
 import com.bitsofproof.supernode.model.TransactionValidationException;
+import com.mysema.query.jpa.impl.JPAQuery;
 
+@Component ("audit")
 public class Audit
 {
 	private static final Logger log = LoggerFactory.getLogger (Audit.class);
+
+	@Autowired
+	private Chain chain;
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Autowired
+	private PlatformTransactionManager transactionManager;
 
 	public void start (String[] args) throws IOException, TransactionValidationException
 	{
@@ -48,7 +75,49 @@ public class Audit
 			log.error ("Invalid options ", e1);
 			return;
 		}
+		new TransactionTemplate (transactionManager).execute (new TransactionCallbackWithoutResult ()
+		{
+			@Override
+			protected void doInTransactionWithoutResult (TransactionStatus status)
+			{
+				status.setRollbackOnly ();
+				try
+				{
+					audit ();
+				}
+				catch ( Exception e )
+				{
+					log.error ("Exception in audit", e);
+				}
+			}
+		});
+	}
 
+	public void audit () throws Exception
+	{
+		long ms = System.currentTimeMillis ();
+		log.info ("Check 1. All heads lead to genesis");
+		validateHeads ();
+		log.info ("Check 1. Done in " + (System.currentTimeMillis () - ms) / 1000.0 + " seconds.");
+		ms = System.currentTimeMillis ();
+	}
+
+	public void validateHeads ()
+	{
+		QHead head = QHead.head;
+		JPAQuery q = new JPAQuery (entityManager);
+		Set<String> checked = new HashSet<String> ();
+		for ( Head h : q.from (head).list (head) )
+		{
+			QBlk blk = QBlk.blk;
+			q = new JPAQuery (entityManager);
+			Blk b = q.from (blk).where (blk.hash.eq (h.getLeaf ())).uniqueResult (blk);
+			while ( b.getPrevious () != null && !checked.contains (b.getHash ()) )
+			{
+				b = b.getPrevious ();
+				checked.add (b.getHash ());
+			}
+		}
 	}
 
 	private static ApplicationContext context;
