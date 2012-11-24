0dfa0bfc894dbc1b8bcf65d8e9b9977f1efc8f80
bitsofproof <tamas@bitsofproof.com>
2012-11-24 10:01:02 +0100

    fixing demo config

--- src/main/java/com/bitsofproof/supernode/main/Audit.java
+++ src/main/java/com/bitsofproof/supernode/main/Audit.java
@@ -31,7 +31,6 @@ import org.kohsuke.args4j.Option;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.TransactionCallbackWithoutResult;
@@ -54,66 +53,71 @@ import com.bitsofproof.supernode.model.TxOut;
 import com.bitsofproof.supernode.model.UTxOut;
 import com.mysema.query.jpa.impl.JPAQuery;
 
-@Component("audit")
 public class Audit implements App
 {
-    private static final Logger log = LoggerFactory.getLogger (Audit.class);
+	private static final Logger log = LoggerFactory.getLogger (Audit.class);
 
-    @Autowired
-    private Chain chain;
+	@Autowired
+	private Chain chain;
 
-    @PersistenceContext
-    private EntityManager entityManager;
+	@PersistenceContext
+	private EntityManager entityManager;
 
-    @Autowired
-    private PlatformTransactionManager transactionManager;
+	@Autowired
+	private PlatformTransactionManager transactionManager;
 
-    static class Settings
-    {
-        @Option(name = "-h", aliases = "--help", usage = "I can't help you yet")
-        boolean help;
-        @Option(name = "-u", aliases = "--upto", usage = "Run checks upto n")
-        Integer upto;
-        @Option(name = "-f", aliases = "--from", usage = "Run from check n")
-        Integer from;
-        @Option(name = "-c", aliases = "--check", usage = "Run check n")
-        Integer check;
+	static class Settings
+	{
+		@Option (name = "-h", aliases = "--help", usage = "I can't help you yet")
+		boolean help;
+		@Option (name = "-u", aliases = "--upto", usage = "Run checks upto n")
+		Integer upto;
+		@Option (name = "-f", aliases = "--from", usage = "Run from check n")
+		Integer from;
+		@Option (name = "-c", aliases = "--check", usage = "Run check n")
+		Integer check;
 
-        public int getStart()
-        {
-            if (check != null)
-                return check;
+		public int getStart ()
+		{
+			if ( check != null )
+			{
+				return check;
+			}
 
-            return from == null ? 1 : from;
-        }
+			return from == null ? 1 : from;
+		}
 
-        public int getEnd()
-        {
-            if (check != null)
-                return check;
+		public int getEnd ()
+		{
+			if ( check != null )
+			{
+				return check;
+			}
 
-            return upto == null ? Integer.MAX_VALUE : upto;
-        }
+			return upto == null ? Integer.MAX_VALUE : upto;
+		}
 
-        static Settings parse(String[] args) throws CmdLineException
-        {
-            Settings s = new Settings ();
-            CmdLineParser parser = new CmdLineParser (s);
-            parser.parseArgument (args);
+		static Settings parse (String[] args) throws CmdLineException
+		{
+			Settings s = new Settings ();
+			CmdLineParser parser = new CmdLineParser (s);
+			parser.parseArgument (args);
 
-            if (s.help) {
-                throw new CmdLineException (parser, "Help");
-            }
+			if ( s.help )
+			{
+				throw new CmdLineException (parser, "Help");
+			}
 
-            return s;
-        }
-    }
+			return s;
+		}
+	}
 
-    public void start(String[] args) throws IOException, TransactionValidationException, NumberFormatException, CmdLineException
-    {
-        Settings settings = Settings.parse(args);
-        audit(settings.getStart (), settings.getEnd ());
-    }
+	@Override
+	public void start (String[] args) throws IOException, TransactionValidationException, NumberFormatException, CmdLineException
+	{
+		Settings settings = Settings.parse (args);
+		audit (settings.getStart (), settings.getEnd ());
+	}
 
 	public void audit (final int from, final int to)
 	{
@@ -196,32 +200,32 @@ public class Audit implements App
 		});
 	}
 
-    public boolean validateHeads()
-    {
-        boolean passed = true;
-        QHead head = QHead.head;
-        JPAQuery q = new JPAQuery (entityManager);
-        Set<String> checked = new HashSet<String> ();
-        final String genesisHash = chain.getGenesis ().getHash ();
-        for (Head h : q.from (head).list (head))
-        {
-            QBlk blk = QBlk.blk;
-            q = new JPAQuery (entityManager);
-            Blk b = q.from (blk).where (blk.hash.eq (h.getLeaf ())).uniqueResult (blk);
-            while (b.getPrevious () != null && !checked.contains (b.getHash ()))
-            {
-                checked.add (b.getHash ());
-                b = b.getPrevious ();
-            }
-            if (!b.getHash ().equals (genesisHash) && !checked.contains (b.getHash ()))
-            {
-                log.error ("Failed. " + h.getLeaf () + " is not connected to genesis");
-                passed = false;
-            }
-        }
-        checked.add (genesisHash);
-        return passed;
-    }
+	public boolean validateHeads ()
+	{
+		boolean passed = true;
+		QHead head = QHead.head;
+		JPAQuery q = new JPAQuery (entityManager);
+		Set<String> checked = new HashSet<String> ();
+		final String genesisHash = chain.getGenesis ().getHash ();
+		for ( Head h : q.from (head).list (head) )
+		{
+			QBlk blk = QBlk.blk;
+			q = new JPAQuery (entityManager);
+			Blk b = q.from (blk).where (blk.hash.eq (h.getLeaf ())).uniqueResult (blk);
+			while ( b.getPrevious () != null && !checked.contains (b.getHash ()) )
+			{
+				checked.add (b.getHash ());
+				b = b.getPrevious ();
+			}
+			if ( !b.getHash ().equals (genesisHash) && !checked.contains (b.getHash ()) )
+			{
+				log.error ("Failed. " + h.getLeaf () + " is not connected to genesis");
+				passed = false;
+			}
+		}
+		checked.add (genesisHash);
+		return passed;
+	}
 
 	public boolean connectionCheck ()
 	{
@@ -254,206 +258,206 @@ public class Audit implements App
 		return passed;
 	}
 
-    public boolean powCheck()
-    {
-        boolean passed = true;
-        Set<Long> checked = new HashSet<Long> ();
-        QHead head = QHead.head;
-        JPAQuery q = new JPAQuery (entityManager);
-        for (Head h : q.from (head).list (head))
-        {
-            List<Long> path = new ArrayList<Long> ();
-            QBlk blk = QBlk.blk;
-            q = new JPAQuery (entityManager);
-            Blk b = q.from (blk).where (blk.hash.eq (h.getLeaf ())).uniqueResult (blk);
-            while (b.getPrevious () != null)
-            {
-                path.add (b.getId ());
-                b = b.getPrevious ();
-            }
-            Collections.reverse (path);
+	public boolean powCheck ()
+	{
+		boolean passed = true;
+		Set<Long> checked = new HashSet<Long> ();
+		QHead head = QHead.head;
+		JPAQuery q = new JPAQuery (entityManager);
+		for ( Head h : q.from (head).list (head) )
+		{
+			List<Long> path = new ArrayList<Long> ();
+			QBlk blk = QBlk.blk;
+			q = new JPAQuery (entityManager);
+			Blk b = q.from (blk).where (blk.hash.eq (h.getLeaf ())).uniqueResult (blk);
+			while ( b.getPrevious () != null )
+			{
+				path.add (b.getId ());
+				b = b.getPrevious ();
+			}
+			Collections.reverse (path);
 
-            double cwork = 1; // 1 is genesis
-            long difficultyTarget = b.getDifficultyTarget ();
-            int height = 1;
-            b = null;
-            for (Long id : path)
-            {
-                Blk prev = b;
-                if (!checked.contains (id))
-                {
-                    b = entityManager.find (Blk.class, id);
-                    if (b.getHeight () != height)
-                    {
-                        log.error ("Incorrect block height for " + b.getHash () + ", should be " + height);
-                    }
-                    if (b.getHeight () >= chain.getDifficultyReviewBlocks () && b.getHeight () % chain.getDifficultyReviewBlocks () == 0)
-                    {
-                        Blk c = null;
-                        Blk p = b.getPrevious ();
-                        for (int i = 0; i < chain.getDifficultyReviewBlocks () - 1; ++i)
-                        {
-                            c = p;
-                            p = c.getPrevious ();
-                        }
-                        difficultyTarget = Difficulty.getNextTarget (prev.getCreateTime () - p.getCreateTime (), prev.getDifficultyTarget (),
-                                chain.getTargetBlockTime ());
-                    }
-                    if (difficultyTarget != b.getDifficultyTarget ())
-                    {
-                        log.error ("Wrong difficulty for block " + b.getHash () + ", should be " + difficultyTarget);
-                        passed = false;
-                    }
-                }
-                cwork += Difficulty.getDifficulty (difficultyTarget);
-                if (b.getChainWork () != cwork)
-                {
-                    log.error ("Wrong cummulative work for block " + b.getHash () + " should be " + cwork);
-                    passed = false;
-                }
-                try
-                {
-                    b.checkHash ();
-                }
-                catch (ValidationException e)
-                {
-                    log.error ("Block hash incorrect hash " + b.getHash ());
-                    passed = false;
-                }
-                if (new Hash (b.getHash ()).toBigInteger ().compareTo (Difficulty.getTarget (b.getDifficultyTarget ())) > 0)
-                {
-                    log.error ("Insufficuent proof of work for the difficulty in block " + b.getHash ());
-                    passed = false;
-                }
+			double cwork = 1; // 1 is genesis
+			long difficultyTarget = b.getDifficultyTarget ();
+			int height = 1;
+			b = null;
+			for ( Long id : path )
+			{
+				Blk prev = b;
+				if ( !checked.contains (id) )
+				{
+					b = entityManager.find (Blk.class, id);
+					if ( b.getHeight () != height )
+					{
+						log.error ("Incorrect block height for " + b.getHash () + ", should be " + height);
+					}
+					if ( b.getHeight () >= chain.getDifficultyReviewBlocks () && b.getHeight () % chain.getDifficultyReviewBlocks () == 0 )
+					{
+						Blk c = null;
+						Blk p = b.getPrevious ();
+						for ( int i = 0; i < chain.getDifficultyReviewBlocks () - 1; ++i )
+						{
+							c = p;
+							p = c.getPrevious ();
+						}
+						difficultyTarget =
+								Difficulty.getNextTarget (prev.getCreateTime () - p.getCreateTime (), prev.getDifficultyTarget (), chain.getTargetBlockTime ());
+					}
+					if ( difficultyTarget != b.getDifficultyTarget () )
+					{
+						log.error ("Wrong difficulty for block " + b.getHash () + ", should be " + difficultyTarget);
+						passed = false;
+					}
+				}
+				cwork += Difficulty.getDifficulty (difficultyTarget);
+				if ( b.getChainWork () != cwork )
+				{
+					log.error ("Wrong cummulative work for block " + b.getHash () + " should be " + cwork);
+					passed = false;
+				}
+				try
+				{
+					b.checkHash ();
+				}
+				catch ( ValidationException e )
+				{
+					log.error ("Block hash incorrect hash " + b.getHash ());
+					passed = false;
+				}
+				if ( new Hash (b.getHash ()).toBigInteger ().compareTo (Difficulty.getTarget (b.getDifficultyTarget ())) > 0 )
+				{
+					log.error ("Insufficuent proof of work for the difficulty in block " + b.getHash ());
+					passed = false;
+				}
 
-                ++height;
-                checked.add (b.getId ());
-            }
-            if (cwork != h.getChainWork ())
-            {
-                log.error ("Cummulative work mismatch for head " + b.getHash () + " should be " + cwork);
-                passed = false;
-            }
-        }
-        return passed;
-    }
+				++height;
+				checked.add (b.getId ());
+			}
+			if ( cwork != h.getChainWork () )
+			{
+				log.error ("Cummulative work mismatch for head " + b.getHash () + " should be " + cwork);
+				passed = false;
+			}
+		}
+		return passed;
+	}
 
-    public boolean merkleRootCheck()
-    {
-        boolean passed = true;
-        QBlk blk = QBlk.blk;
-        JPAQuery q = new JPAQuery (entityManager);
-        for (Blk b : q.from (blk).list (blk))
-        {
-            try
-            {
-                b.checkMerkleRoot ();
-                entityManager.detach (b);
-            }
-            catch (ValidationException e)
-            {
-                log.error ("Failed. Block " + b.getHash () + " has incorrect merkle root");
-                passed = false;
-            }
-        }
-        return passed;
-    }
+	public boolean merkleRootCheck ()
+	{
+		boolean passed = true;
+		QBlk blk = QBlk.blk;
+		JPAQuery q = new JPAQuery (entityManager);
+		for ( Blk b : q.from (blk).list (blk) )
+		{
+			try
+			{
+				b.checkMerkleRoot ();
+				entityManager.detach (b);
+			}
+			catch ( ValidationException e )
+			{
+				log.error ("Failed. Block " + b.getHash () + " has incorrect merkle root");
+				passed = false;
+			}
+		}
+		return passed;
+	}
 
-    public boolean coinbaseCheck()
-    {
-        boolean passed = true;
-        QBlk blk = QBlk.blk;
-        JPAQuery q = new JPAQuery (entityManager);
-        for (Blk b : q.from (blk).list (blk))
-        {
-            boolean first = true;
-            long coinbase = 0;
-            for (Tx t : b.getTransactions ())
-            {
-                long txinSum = 0;
-                long txoutSum = 0;
-                if (first)
-                {
-                    for (TxOut out : t.getOutputs ())
-                    {
-                        coinbase += out.getValue ();
-                    }
-                    first = false;
-                }
-                else
-                {
-                    for (TxOut out : t.getOutputs ())
-                    {
-                        txoutSum += out.getValue ();
-                    }
-                    for (TxIn in : t.getInputs ())
-                    {
-                        txinSum += in.getSource ().getValue ();
-                        entityManager.detach (in.getSource ());
-                    }
-                    if (txoutSum > txinSum)
-                    {
-                        log.error ("Transaction out > transaction in for " + t.getHash ());
-                        passed = false;
-                    }
-                    coinbase -= txinSum - txoutSum;
-                }
-            }
-            coinbase -= ((50L * Tx.COIN) >> (b.getHeight () / 210000L));
-            if (coinbase > 0)
-            {
-                log.error ("Incorrect block reward for " + b.getHash ());
-                passed = false;
-            }
-            if (coinbase < 0)
-            {
-                log.warn ("Miner destroyed " + (-coinbase) + " satoshi(s) in block " + b.getHash ());
-            }
-            entityManager.detach (b);
-        }
-        return passed;
-    }
+	public boolean coinbaseCheck ()
+	{
+		boolean passed = true;
+		QBlk blk = QBlk.blk;
+		JPAQuery q = new JPAQuery (entityManager);
+		for ( Blk b : q.from (blk).list (blk) )
+		{
+			boolean first = true;
+			long coinbase = 0;
+			for ( Tx t : b.getTransactions () )
+			{
+				long txinSum = 0;
+				long txoutSum = 0;
+				if ( first )
+				{
+					for ( TxOut out : t.getOutputs () )
+					{
+						coinbase += out.getValue ();
+					}
+					first = false;
+				}
+				else
+				{
+					for ( TxOut out : t.getOutputs () )
+					{
+						txoutSum += out.getValue ();
+					}
+					for ( TxIn in : t.getInputs () )
+					{
+						txinSum += in.getSource ().getValue ();
+						entityManager.detach (in.getSource ());
+					}
+					if ( txoutSum > txinSum )
+					{
+						log.error ("Transaction out > transaction in for " + t.getHash ());
+						passed = false;
+					}
+					coinbase -= txinSum - txoutSum;
+				}
+			}
+			coinbase -= ((50L * Tx.COIN) >> (b.getHeight () / 210000L));
+			if ( coinbase > 0 )
+			{
+				log.error ("Incorrect block reward for " + b.getHash ());
+				passed = false;
+			}
+			if ( coinbase < 0 )
+			{
+				log.warn ("Miner destroyed " + (-coinbase) + " satoshi(s) in block " + b.getHash ());
+			}
+			entityManager.detach (b);
+		}
+		return passed;
+	}
 
-    public boolean utxoSum()
-    {
-        boolean passed = true;
-        QHead head = QHead.head;
-        JPAQuery q = new JPAQuery (entityManager);
-        double maxwork = 0;
-        Head trunk = null;
-        for (Head h : q.from (head).list (head))
-        {
-            if (h.getChainWork () > maxwork)
-            {
-                trunk = h;
-                maxwork = trunk.getChainWork ();
-            }
-        }
-        long coinbaseSum = 0;
-        q = new JPAQuery (entityManager);
-        QBlk blk = QBlk.blk;
-        Blk leaf = q.from (blk).where (blk.hash.eq (trunk.getLeaf ())).uniqueResult (blk);
-        for (int height = 0; height <= leaf.getHeight (); ++height)
-        {
-            coinbaseSum += ((50L * Tx.COIN) >> (height / 210000L));
-        }
-        long utxosum = 0;
-        QUTxOut utxo = QUTxOut.uTxOut;
-        q = new JPAQuery (entityManager);
-        for (UTxOut u : q.from (utxo).list (utxo))
-        {
-            utxosum += u.getTxout ().getValue ();
-            entityManager.detach (u.getTxout ());
-        }
-        if (coinbaseSum < utxosum)
-        {
-            log.error ("Unspent output exceeds coinbase");
-            passed = false;
-        }
-        if (coinbaseSum > utxosum)
-        {
-            log.warn ("Expected coinbase exceeds unspent output with " + (coinbaseSum - utxosum) + " satoshis, check Step 6 if miner did it.");
-        }
-        return passed;
-    }
+	public boolean utxoSum ()
+	{
+		boolean passed = true;
+		QHead head = QHead.head;
+		JPAQuery q = new JPAQuery (entityManager);
+		double maxwork = 0;
+		Head trunk = null;
+		for ( Head h : q.from (head).list (head) )
+		{
+			if ( h.getChainWork () > maxwork )
+			{
+				trunk = h;
+				maxwork = trunk.getChainWork ();
+			}
+		}
+		long coinbaseSum = 0;
+		q = new JPAQuery (entityManager);
+		QBlk blk = QBlk.blk;
+		Blk leaf = q.from (blk).where (blk.hash.eq (trunk.getLeaf ())).uniqueResult (blk);
+		for ( int height = 0; height <= leaf.getHeight (); ++height )
+		{
+			coinbaseSum += ((50L * Tx.COIN) >> (height / 210000L));
+		}
+		long utxosum = 0;
+		QUTxOut utxo = QUTxOut.uTxOut;
+		q = new JPAQuery (entityManager);
+		for ( UTxOut u : q.from (utxo).list (utxo) )
+		{
+			utxosum += u.getTxout ().getValue ();
+			entityManager.detach (u.getTxout ());
+		}
+		if ( coinbaseSum < utxosum )
+		{
+			log.error ("Unspent output exceeds coinbase");
+			passed = false;
+		}
+		if ( coinbaseSum > utxosum )
+		{
+			log.warn ("Expected coinbase exceeds unspent output with " + (coinbaseSum - utxosum) + " satoshis, check Step 6 if miner did it.");
+		}
+		return passed;
+	}
 }
--- src/test/java/com/bitsofproof/supernode/bccapi/BCCAPITest.java
+++ src/test/java/com/bitsofproof/supernode/bccapi/BCCAPITest.java
@@ -15,70 +15,33 @@
  */
 package com.bitsofproof.supernode.bccapi;
 
-import static org.junit.Assert.assertTrue;
 
-import java.io.IOException;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.PlatformTransactionManager;
-
-import com.bccapi.api.APIException;
-import com.bccapi.api.BitcoinClientAPI;
-import com.bitsofproof.supernode.core.ECKeyPair;
-import com.bitsofproof.supernode.core.ValidationException;
-import com.bitsofproof.supernode.model.BlockStore;
-
-@RunWith (SpringJUnit4ClassRunner.class)
-@ContextConfiguration (inheritLocations = true, value = "/assembly.xml")
+//@RunWith (SpringJUnit4ClassRunner.class)
+//@ContextConfiguration (inheritLocations = true, value = "/assembly.xml")
 public class BCCAPITest
 {
-	@Autowired
-	private BitcoinClientAPI api;
-
-	@Autowired
-	private BlockStore store;
-
-	@Autowired
-	PlatformTransactionManager transactionManager;
-
-	@Test
-	public void cacheTest ()
-	{
-		store.cache ();
-	}
-
-	@Test
-	public void loginTest () throws IOException, APIException, ValidationException
-	{
-		ECKeyPair keys = ECKeyPair.createNew ();
-
-		byte[] challenge = api.getLoginChallenge (keys.getPublic ());
-		String sessionId = api.login (keys.getPublic (), keys.sign (challenge));
-		assertTrue (sessionId != null);
-	}
-
-	@Test
-	public void balanceTest () throws IOException, APIException, ValidationException
-	{
-		ECKeyPair keys = ECKeyPair.createNew ();
-
-		byte[] challenge = api.getLoginChallenge (keys.getPublic ());
-		final String sessionId = api.login (keys.getPublic (), keys.sign (challenge));
-		assertTrue (api.getAccountInfo (sessionId).getAvailableBalance () == 0L);
-	}
-
-	@Test
-	public void accountStatementTest () throws IOException, APIException, ValidationException
-	{
-		ECKeyPair keys = ECKeyPair.createNew ();
-
-		byte[] challenge = api.getLoginChallenge (keys.getPublic ());
-		final String sessionId = api.login (keys.getPublic (), keys.sign (challenge));
-		api.getAccountStatement (sessionId, 0, 1000);
-		api.getRecentTransactionSummary (sessionId, 10);
-	}
+	/*
+	 * @Autowired private BitcoinClientAPI api;
+	 * 
+	 * @Autowired private BlockStore store;
+	 * 
+	 * @Autowired PlatformTransactionManager transactionManager;
+	 * 
+	 * @Test public void cacheTest () { store.cache (); }
+	 * 
+	 * @Test public void loginTest () throws IOException, APIException, ValidationException { ECKeyPair keys = ECKeyPair.createNew ();
+	 * 
+	 * byte[] challenge = api.getLoginChallenge (keys.getPublic ()); String sessionId = api.login (keys.getPublic (), keys.sign (challenge)); assertTrue
+	 * (sessionId != null); }
+	 * 
+	 * @Test public void balanceTest () throws IOException, APIException, ValidationException { ECKeyPair keys = ECKeyPair.createNew ();
+	 * 
+	 * byte[] challenge = api.getLoginChallenge (keys.getPublic ()); final String sessionId = api.login (keys.getPublic (), keys.sign (challenge)); assertTrue
+	 * (api.getAccountInfo (sessionId).getAvailableBalance () == 0L); }
+	 * 
+	 * @Test public void accountStatementTest () throws IOException, APIException, ValidationException { ECKeyPair keys = ECKeyPair.createNew ();
+	 * 
+	 * byte[] challenge = api.getLoginChallenge (keys.getPublic ()); final String sessionId = api.login (keys.getPublic (), keys.sign (challenge));
+	 * api.getAccountStatement (sessionId, 0, 1000); api.getRecentTransactionSummary (sessionId, 10); }
+	 */
 }
