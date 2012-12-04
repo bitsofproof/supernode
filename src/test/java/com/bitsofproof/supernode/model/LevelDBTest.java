f99b849b4b70c757adf7c579396414292eba0402
bitsofproof <tamas@bitsofproof.com>
2012-12-04 08:48:51 +0100

    more leveldb tests

--- src/test/java/com/bitsofproof/supernode/model/LevelDBTest.java
+++ src/test/java/com/bitsofproof/supernode/model/LevelDBTest.java
@@ -7,30 +7,83 @@ import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
+import org.fusesource.leveldbjni.JniDBFactory;
 import org.iq80.leveldb.DB;
+import org.iq80.leveldb.DBComparator;
 import org.iq80.leveldb.DBException;
+import org.iq80.leveldb.DBIterator;
 import org.iq80.leveldb.Options;
+import org.iq80.leveldb.ReadOptions;
 import org.iq80.leveldb.WriteBatch;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LevelDBTest
 {
+	private static final Logger log = LoggerFactory.getLogger (LevelDBTest.class);
+
 	private static DB db;
+
+	private static final String testdb = "levelDBExample";
 
 	@BeforeClass
 	public static void openDB () throws IOException
 	{
+		DBComparator comparator = new DBComparator ()
+		{
+			@Override
+			public int compare (byte[] key1, byte[] key2)
+			{
+				return new String (key1).compareTo (new String (key2));
+			}
+
+			@Override
+			public String name ()
+			{
+				return "simple";
+			}
+
+			@Override
+			public byte[] findShortestSeparator (byte[] start, byte[] limit)
+			{
+				return start;
+			}
+
+			@Override
+			public byte[] findShortSuccessor (byte[] key)
+			{
+				return key;
+			}
+		};
+		org.iq80.leveldb.Logger logger = new org.iq80.leveldb.Logger ()
+		{
+			@Override
+			public void log (String message)
+			{
+				log.trace (message);
+			}
+		};
 		Options options = new Options ();
+		options.comparator (comparator);
+		options.logger (logger);
+		options.cacheSize (100 * 1048576); // 100MB cache
+		JniDBFactory.pushMemoryPool (100 * 1048576);
 		options.createIfMissing (true);
-		db = factory.open (new File ("levelDBExample"), options);
+		db = factory.open (new File (testdb), options);
 	}
 
 	@AfterClass
-	public static void closeDB ()
+	public static void closeDB () throws IOException
 	{
+		String stats = db.getProperty ("leveldb.stats");
+		System.out.println (stats);
 		db.close ();
+		Options options = new Options ();
+		factory.destroy (new File (testdb), options);
+		JniDBFactory.popMemoryPool ();
 	}
 
 	@Test
@@ -53,5 +106,51 @@ public class LevelDBTest
 
 		db.write (batch);
 		assertTrue (new String (db.get ("London".getBytes ()), "US-ASCII").equals ("red"));
+	}
+
+	@Test
+	public void iteratorTest () throws UnsupportedEncodingException
+	{
+		DBIterator iterator = db.iterator ();
+		doIterate (iterator);
+	}
+
+	private void doIterate (DBIterator iterator) throws UnsupportedEncodingException
+	{
+		try
+		{
+			String[] keys = { "London", "Tampa" };
+			String[] values = { "red", "green" };
+			int i = 0;
+			for ( iterator.seekToFirst (); iterator.hasNext (); iterator.next () )
+			{
+				String key = new String (iterator.peekNext ().getKey (), "US-ASCII");
+				String value = new String (iterator.peekNext ().getValue (), "US-ASCII");
+				assertTrue (keys[i].equals (key));
+				assertTrue (values[i].equals (value));
+				++i;
+			}
+		}
+		finally
+		{
+			// Make sure you close the iterator to avoid resource leaks.
+			iterator.close ();
+		}
+	}
+
+	@Test
+	public void snapshotTest () throws UnsupportedEncodingException
+	{
+		ReadOptions ro = new ReadOptions ();
+		ro.snapshot (db.getSnapshot ());
+		try
+		{
+			doIterate (db.iterator (ro));
+		}
+		finally
+		{
+			// Make sure you close the snapshot to avoid resource leaks.
+			ro.snapshot ().close ();
+		}
 	}
 }
