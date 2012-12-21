92bf228eccac9b7ec5a4c7f5fd48bd24c3540eb0
bitsofproof <tamas@bitsofproof.com>
2012-12-21 09:09:33 +0100

    debug

--- src/main/java/com/bitsofproof/supernode/model/LvlStore.java
+++ src/main/java/com/bitsofproof/supernode/model/LvlStore.java
@@ -698,6 +698,7 @@ public class LvlStore extends CachedBlockStore implements Discovery, PeerStore
 	{
 		List<TxIn> result = new ArrayList<TxIn> ();
 		List<Tx> related = readRelatedTx (addresses);
+		log.trace ("related " + related.size ());
 		for ( Tx t : related )
 		{
 			for ( TxIn i : t.getInputs () )
@@ -705,13 +706,16 @@ public class LvlStore extends CachedBlockStore implements Discovery, PeerStore
 				if ( !i.getSourceHash ().equals (Hash.ZERO_HASH_STRING) )
 				{
 					Tx s = readTx (i.getSourceHash ());
+					log.trace ("related tx " + s.getHash ());
 					Blk b = readBlk (s.getBlockHash (), false);
 					if ( b.getCreateTime () >= from )
 					{
+						log.trace ("related tx in time window " + s.getHash ());
 						TxOut spend = s.getOutputs ().get (i.getIx ().intValue ());
 						i.setSource (spend);
 						if ( addresses.contains (spend.getOwner1 ()) || addresses.contains (spend.getOwner2 ()) || addresses.contains (spend.getOwner3 ()) )
 						{
+							log.trace ("related tx with address " + s.getHash ());
 							i.setTransaction (t);
 							t.setBlock (b);
 							i.setBlockTime (b.getCreateTime ());
