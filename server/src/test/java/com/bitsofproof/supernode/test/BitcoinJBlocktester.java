7f628cd0bcf41ec898df2fcd59bd66988df49ef1
bitsofproof <tamas@bitsofproof.com>
2013-02-23 21:31:41 +0100

    blocktester added (empty at the moment)

--- server/src/test/java/com/bitsofproof/supernode/test/BitcoinJBlocktester.java
+++ server/src/test/java/com/bitsofproof/supernode/test/BitcoinJBlocktester.java
@@ -19,7 +19,8 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -34,7 +35,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.bitsofproof.supernode.api.ValidationException;
 import com.bitsofproof.supernode.core.BlockStore;
-import com.bitsofproof.supernode.core.Chain;
+import com.bitsofproof.supernode.core.BlocktesterChain;
 import com.bitsofproof.supernode.model.Blk;
 import com.bitsofproof.supernode.model.Tx;
 import com.bitsofproof.supernode.model.TxIn;
@@ -69,151 +70,50 @@ public class BitcoinJBlocktester
 	@Test
 	public void bitcoindValidTxTest () throws IOException, JSONException, ValidationException
 	{
+		BlocktesterChain chain = new BlocktesterChain ();
+		store.resetStore (chain);
+		store.cache (chain, 0);
+
+		Map<String, Blk> pending = new HashMap<String, Blk> ();
+
 		JSONArray testData = readObjectArray (BITCOINJ_BLOCKTESTS);
 		for ( int i = 0; i < testData.length (); ++i )
 		{
 			JSONObject test = testData.getJSONObject (i);
-			log.info ("Test " + test.getString ("name"));
-			Blk blk = deserializeBlock (test.getString ("rawblock"));
-			if ( i == 0 )
+			String rawblock = test.getString ("rawblock");
+			String expectTrunk = test.getString ("trunk");
+			String name = test.getString ("name");
+			if ( rawblock.length () < 2000000 ) // this is checked at higher level
 			{
-				initdb (blk);
-			}
-			else
-			{
-				// execute some of bitcoinj tests
-				// b7 is changed to reject since it is double spend
-				// bitcoinj however accepts it since it is on a shorter
-				// orphan path
-				boolean accepted = false;
-				boolean shouldAccept = test.getBoolean ("accept");
+				Blk blk = deserializeBlock (rawblock);
 				try
 				{
-					store.storeBlock (blk);
-					log.trace ("accepted block " + blk.getHash ());
-					accepted = true;
-				}
-				catch ( Exception e )
-				{
-					if ( shouldAccept )
+					if ( store.isStoredBlock (blk.getPreviousHash ()) )
 					{
-						log.error ("rejected block " + blk.getHash ());
+						store.storeBlock (blk);
+						String hash = blk.getHash ();
+						while ( pending.containsKey (hash) )
+						{
+							Blk b = pending.get (hash);
+							store.storeBlock (b); // out of order blocks checked at higher level
+							hash = b.getHash ();
+						}
+
+						log.trace ("accepted " + name + " " + blk.getHash ());
 					}
 					else
 					{
-						log.trace ("rejected block " + blk.getHash ());
+						pending.put (blk.getPreviousHash (), blk);
+						log.trace ("pending " + name + " " + blk.getHash ());
 					}
 				}
-				assertTrue (accepted == shouldAccept);
-				if ( !test.getString ("name").equals ("b12") )
+				catch ( Exception e )
 				{
-					// b12 expects a trunk in bitcoinj test that
-					// would only be the case if I would store
-					// blocks without parent, then resolve as
-					// parent arrives. This logic happens in
-					// bitsofproof on a higher level, that is
-					// the chain loader.
-					// therefore result would be same, but not
-					// in this unit test.
-					String expectTrunk = test.getString ("trunk");
-					String haveTrunk = store.getHeadHash ();
-					if ( !expectTrunk.equals (haveTrunk) )
-					{
-						log.error ("Mismatch in trunk expected " + expectTrunk + " have " + haveTrunk);
-					}
-					assertTrue (expectTrunk.equals (haveTrunk));
+					log.trace ("rejected " + name + " " + blk.getHash ());
 				}
 			}
+			assertTrue (expectTrunk.equals (store.getHeadHash ()));
 		}
-	}
-
-	private void initdb (final Blk genesis) throws ValidationException
-	{
-		Chain chain = new Chain ()
-		{
-
-			@Override
-			public BigInteger getMinimumTarget ()
-			{
-				return BigInteger.valueOf (1).shiftLeft (256).subtract (BigInteger.ONE);
-			}
-
-			@Override
-			public long getRewardForHeight (int height)
-			{
-				return 5000000000L;
-			}
-
-			@Override
-			public int getDifficultyReviewBlocks ()
-			{
-				return 2016;
-			}
-
-			@Override
-			public int getTargetBlockTime ()
-			{
-				return 1209600;
-			}
-
-			@Override
-			public boolean isProduction ()
-			{
-				return true;
-			}
-
-			@Override
-			public int getAddressFlag ()
-			{
-				return 0;
-			}
-
-			@Override
-			public int getMultisigAddressFlag ()
-			{
-				return 0;
-			}
-
-			@Override
-			public Blk getGenesis ()
-			{
-				return genesis;
-			}
-
-			@Override
-			public long getMagic ()
-			{
-				return 0;
-			}
-
-			@Override
-			public int getPort ()
-			{
-				return 0;
-			}
-
-			@Override
-			public byte[] getAlertKey ()
-			{
-				return null;
-			}
-
-			@Override
-			public long getVersion ()
-			{
-				return 1;
-			}
-
-			@Override
-			public boolean isUnitTest ()
-			{
-				return true;
-			}
-		};
-		// this test can spend genesis
-		genesis.getTransactions ().get (0).getOutputs ().get (0).setAvailable (true);
-		store.resetStore (chain);
-		store.cache (chain, 0);
 	}
 
 	private Blk deserializeBlock (String s)
