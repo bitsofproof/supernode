1336aa6d3fa4639147b11f4a344cb36f14c5b576
bitsofproof <tamas@bitsofproof.com>
2013-02-25 20:47:14 +0100

    added send block test

--- server/src/test/java/com/bitsofproof/supernode/test/APITest.java
+++ server/src/test/java/com/bitsofproof/supernode/test/APITest.java
@@ -2,6 +2,9 @@ package com.bitsofproof.supernode.test;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+import java.util.concurrent.Semaphore;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -11,6 +14,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.bitsofproof.supernode.api.BCSAPI;
+import com.bitsofproof.supernode.api.Block;
+import com.bitsofproof.supernode.api.TrunkListener;
 import com.bitsofproof.supernode.api.ValidationException;
 import com.bitsofproof.supernode.core.BlockStore;
 import com.bitsofproof.supernode.core.Chain;
@@ -30,6 +35,8 @@ public class APITest
 	@Autowired
 	BCSAPI api;
 
+	private final Semaphore ready = new Semaphore (0);
+
 	@Test
 	public void init () throws ValidationException
 	{
@@ -40,7 +47,38 @@ public class APITest
 	@Test
 	public void checkGenesis ()
 	{
-		assertTrue (api.getBlock (chain.getGenesis ().getHash ()).getHash ().equals (chain.getGenesis ().getHash ()));
+		String genesisHash = chain.getGenesis ().getHash ();
+		assertTrue (api.getBlock (genesisHash).getHash ().equals (genesisHash));
 	}
 
+	@Test
+	public void sendBlock ()
+	{
+		Block block =
+				Block.fromWireDump ("0100000006226e46111a0b59caaf126043eb5bbf28c34f3a5e332a1fc7b2b73cf188910f9898befce8d1310a0f2c470f5b924fda4106715859ef0bfe157fd67abd3cacf1072d2a51ffff7f20000000000101000000010000000000000000000000000000000000000000000000000000000000000000ffffffff020001ffffffff0100f2052a010000002321031cd2dfbdbd6d50991a022619dba47aac054c1b3b9cf5cf0186093e5c3010ded9ac00000000");
+
+		final String hash = block.getHash ();
+
+		api.registerTrunkListener (new TrunkListener ()
+		{
+			@Override
+			public void trunkUpdate (List<Block> removed, List<Block> added)
+			{
+				Block got = added.get (0);
+				got.computeHash ();
+				assertTrue (got.getHash ().equals (hash));
+				ready.release ();
+			}
+		});
+
+		api.sendBlock (block);
+		ready.acquireUninterruptibly ();
+		try
+		{
+			Thread.sleep (1000);
+		}
+		catch ( InterruptedException e )
+		{
+		}
+	}
 }
