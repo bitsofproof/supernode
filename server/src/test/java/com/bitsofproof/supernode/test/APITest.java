ea21b50ec624a70e10ad6aa2bcee72241f0cebb1
bitsofproof <tamas@bitsofproof.com>
2013-04-25 09:54:46 +0200

    further tests

--- server/src/test/java/com/bitsofproof/supernode/test/APITest.java
+++ server/src/test/java/com/bitsofproof/supernode/test/APITest.java
@@ -7,6 +7,7 @@ import java.math.BigInteger;
 import java.security.Security;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Semaphore;
@@ -25,9 +26,12 @@ import com.bitsofproof.supernode.api.AccountManager;
 import com.bitsofproof.supernode.api.BCSAPI;
 import com.bitsofproof.supernode.api.BCSAPIException;
 import com.bitsofproof.supernode.api.Block;
+import com.bitsofproof.supernode.api.Color;
 import com.bitsofproof.supernode.api.Hash;
+import com.bitsofproof.supernode.api.Key;
 import com.bitsofproof.supernode.api.SerializedWallet;
 import com.bitsofproof.supernode.api.Transaction;
+import com.bitsofproof.supernode.api.TransactionListener;
 import com.bitsofproof.supernode.api.TrunkListener;
 import com.bitsofproof.supernode.api.ValidationException;
 import com.bitsofproof.supernode.core.BlockStore;
@@ -66,7 +70,7 @@ public class APITest
 		}
 
 		@Override
-		public synchronized void accountChanged (AccountManager account)
+		public void accountChanged (AccountManager account)
 		{
 			assertTrue (account.getName ().equals (name));
 			ready.release ();
@@ -85,8 +89,94 @@ public class APITest
 		}
 	}
 
+	public static class ValidationMonitor implements TrunkListener, TransactionListener
+	{
+		private final Semaphore blockValidated = new Semaphore (0);
+		private final Semaphore transactionValidated = new Semaphore (0);
+		private String last;
+		private final List<Transaction> unconfirmed = new ArrayList<Transaction> ();
+
+		@Override
+		public void trunkUpdate (List<Block> removed, List<Block> added)
+		{
+			if ( added != null )
+			{
+				for ( Block b : added )
+				{
+					b.computeHash ();
+					last = b.getHash ();
+					Iterator<Transaction> i = unconfirmed.iterator ();
+					while ( i.hasNext () )
+					{
+						Transaction t = i.next ();
+						for ( Transaction tb : b.getTransactions () )
+						{
+							if ( tb.getHash ().equals (t.getHash ()) )
+							{
+								i.remove ();
+							}
+						}
+					}
+				}
+			}
+			if ( removed != null )
+			{
+				for ( Block b : added )
+				{
+					b.computeHash ();
+					unconfirmed.addAll (b.getTransactions ());
+				}
+			}
+
+			blockValidated.release ();
+		}
+
+		public List<Transaction> getUnconfirmed ()
+		{
+			return unconfirmed;
+		}
+
+		public String getLast ()
+		{
+			return last;
+		}
+
+		public void expectBlockValidations (int n)
+		{
+			try
+			{
+				assertTrue (blockValidated.tryAcquire (n, 1, TimeUnit.SECONDS));
+				assertFalse (blockValidated.tryAcquire ());
+			}
+			catch ( InterruptedException e )
+			{
+			}
+		}
+
+		public void expectTransactionValidations (int n)
+		{
+			try
+			{
+				assertTrue (transactionValidated.tryAcquire (n, 1, TimeUnit.SECONDS));
+				assertFalse (transactionValidated.tryAcquire ());
+			}
+			catch ( InterruptedException e )
+			{
+			}
+		}
+
+		@Override
+		public void process (Transaction t)
+		{
+			unconfirmed.add (t);
+			transactionValidated.release ();
+		}
+	}
+
 	private static final AccountMonitor bobMonitor = new AccountMonitor ("Bob");
 	private static final AccountMonitor aliceMonitor = new AccountMonitor ("Alice");
+
+	private static final ValidationMonitor validationMonitor = new ValidationMonitor ();
 
 	@BeforeClass
 	public static void provider ()
@@ -106,6 +196,9 @@ public class APITest
 
 		alice.addAccountListener (aliceMonitor);
 		bob.addAccountListener (bobMonitor);
+
+		api.registerTrunkListener (validationMonitor);
+		api.registerTransactionListener (validationMonitor);
 	}
 
 	@Test
@@ -122,53 +215,16 @@ public class APITest
 		mineBlock (block);
 		blocks.put (1, block);
 
-		final String hash = block.getHash ();
-
-		final Semaphore ready = new Semaphore (0);
-
-		TrunkListener listener = new TrunkListener ()
-		{
-			@Override
-			public void trunkUpdate (List<Block> removed, List<Block> added)
-			{
-				Block got = added.get (0);
-				got.computeHash ();
-				assertTrue (got.getHash ().equals (hash));
-				ready.release ();
-			}
-		};
-
-		api.registerTrunkListener (listener);
-
 		api.sendBlock (block);
 
-		try
-		{
-			assertTrue (ready.tryAcquire (2, TimeUnit.SECONDS));
-			api.removeTrunkListener (listener);
-		}
-		catch ( InterruptedException e )
-		{
-		}
+		validationMonitor.expectBlockValidations (1);
+		assertTrue (validationMonitor.getLast ().equals (block.getHash ()));
 		aliceMonitor.expectUpdates (1);
 	}
 
 	@Test
 	public void send10Blocks () throws ValidationException, BCSAPIException
 	{
-		final Semaphore ready = new Semaphore (0);
-
-		TrunkListener listener = new TrunkListener ()
-		{
-			@Override
-			public void trunkUpdate (List<Block> removed, List<Block> added)
-			{
-				ready.release (added.size ());
-			}
-		};
-
-		api.registerTrunkListener (listener);
-
 		String hash = blocks.get (1).getHash ();
 		for ( int i = 0; i < 10; ++i )
 		{
@@ -179,15 +235,8 @@ public class APITest
 			hash = block.getHash ();
 			api.sendBlock (block);
 		}
-
-		try
-		{
-			assertTrue (ready.tryAcquire (10, 2, TimeUnit.SECONDS));
-			api.removeTrunkListener (listener);
-		}
-		catch ( InterruptedException e )
-		{
-		}
+		validationMonitor.expectBlockValidations (10);
+		assertTrue (validationMonitor.getLast ().equals (hash));
 		aliceMonitor.expectUpdates (10);
 	}
 
@@ -199,8 +248,47 @@ public class APITest
 		api.sendTransaction (spend);
 		aliceMonitor.expectUpdates (1);
 		bobMonitor.expectUpdates (1);
+		validationMonitor.expectTransactionValidations (1);
 		assertTrue (bob.getBalance () == 50 * COIN);
 		assertTrue (alice.getBalance () == aliceStartingBalance - bob.getBalance () - FEE);
+	}
+
+	@Test
+	public void splitSome () throws BCSAPIException, ValidationException
+	{
+		long aliceStartingBalance = alice.getBalance ();
+		Transaction spend = alice.split (new long[] { 1 * COIN, 2 * COIN }, FEE);
+		api.sendTransaction (spend);
+		aliceMonitor.expectUpdates (1);
+		validationMonitor.expectTransactionValidations (1);
+		assertTrue (alice.getBalance () == aliceStartingBalance - FEE);
+	}
+
+	@Test
+	public void createColor () throws BCSAPIException, ValidationException
+	{
+		long aliceStartingBalance = alice.getBalance ();
+		Transaction genesis = alice.createColorGenesis (10, COIN, FEE);
+		api.sendTransaction (genesis);
+		aliceMonitor.expectUpdates (1);
+		validationMonitor.expectTransactionValidations (1);
+		assertTrue (alice.getBalance () == aliceStartingBalance - FEE);
+
+		Block block = createBlock (blocks.get (11).getHash (), Transaction.createCoinbase (alice.getNextKey (), COIN, 12));
+		block.setCreateTime (block.getCreateTime () + 11 * 1000); // avoid clash of timestamp with median
+		block.getTransactions ().addAll (validationMonitor.getUnconfirmed ());
+		mineBlock (block);
+		blocks.put (12, block);
+		api.sendBlock (block);
+
+		Color color = new Color ();
+		color.setTransaction (genesis.getHash ());
+		color.setTerms ("I will deliver you blue");
+		color.setUnit (COIN);
+		Key key = alice.getKeyForAddress (genesis.getOutputs ().get (0).getOutputAddress ());
+		color.setPubkey (key.getPublic ());
+		color.sign (key);
+		api.issueColor (color);
 	}
 
 	private Block createBlock (String previous, Transaction coinbase)
